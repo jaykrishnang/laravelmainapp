a62fd5ddfc8cbf4a9f51b62cc8e9759658886a62
qburst-jaykrishnang <jay@qburst.com>
2024-09-11 18:09:24 +0530

    Delete, Policy

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::post('/logout', [UserController::class, "logout"])->middleware('mustBeLog
 Route::get('/create-post', [PostController::class, "showCreateForm"])->middleware('mustBeLoggedIn');
 Route::post('/create-post', [PostController::class, "storeNewPost"])->middleware('mustBeLoggedIn');
 Route::get('/post/{post}', [PostController::class, "viewSinglePost"])->middleware('mustBeLoggedIn');
+Route::delete('/post/{post}', [PostController::class, "delete"]);
 
 //Profile Related Routes
 Route::get('/profile/{user:username}', [UserController::class, "profile"]);
